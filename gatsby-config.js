c6bed8082f870f9f012a2ae4d85756aca06b591e
xissshawww <xissshawww@gmail.com>
2021-12-20 11:48:21 +0800

    x

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,17 @@
 module.exports = {
+  siteMetadata: {
+    title: 'spinoza',
+    description: `spinoz-`,
+    siteUrl: 'https://deep-swarm.xyz/spinoza',
+  },
   plugins: [
     {
       resolve: `gatsby-theme-garden`,
       options: {
+        basePath: `/spinoza`,
         contentPath: `${__dirname}/content/garden`,
-        rootNote: `/hello`,
+        rootNote: `/spinoza/hello`,
       },
     },
   ],
-  siteMetadata: {
-    title: `Site title`,
-  },
 }
